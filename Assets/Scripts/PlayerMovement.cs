470e2d3de6440b04590f8c8173c68f446939d4f1
godofnanners <casper.martensson@telia.com>
2018-04-13 16:27:34 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D playerRB;
+    // Update is called once per frame
+
+    private void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        playerRB.AddForce(movement*speed);
+    }
+}
---
+++ Assets/Scripts/SwordHits.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SwordHits : MonoBehaviour {
+
+    [SerializeField]private SpriteRenderer swordsprite;
+    [SerializeField]bool hitDelay;
+    [SerializeField] float activeHitTime;
+    Color originalColor;
+
+	// Use this for initialization
+	void Start () {
+        originalColor = swordsprite.color;
+        hitDelay = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate (){
+
+        swordsprite.color = originalColor;
+
+        if (Input.GetButton("Hit") && hitDelay==false)
+        {
+            swordsprite.color = Color.red;
+            hitDelay = true;
+        }
+
+        StartCoroutine(ActivityTime(activeHitTime));
+        
+        
+    }
+
+    IEnumerator ActivityTime(float time)
+    {
+        if (hitDelay)
+        {
+            yield return new WaitForSeconds(time);
+            hitDelay = false;
+            
+        }
+            
+    }
+}
